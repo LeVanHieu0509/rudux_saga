321c99248374524928ff2f6de8cd4de6d266dd81
whoant <vovanhoangtuan4.2@gmail.com>
2022-03-06 17:14:39 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import cityApi from 'api/cityApi';
-import studentApi from 'api/studentApi';
-import { Route, Routes } from 'react-router-dom';
-import { LoginPage } from 'features/auth/pages/LoginPage';
-import { AdminLayout } from 'components/Layout';
-import PrivateRoute from 'components/Common/PrivateRoute';
-import NotFound from 'components/Common/NotFound';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import cityApi from "api/cityApi";
+import studentApi from "api/studentApi";
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "features/auth/pages/LoginPage";
+import { AdminLayout } from "components/Layout";
+import PrivateRoute from "components/Common/PrivateRoute";
+import NotFound from "components/Common/NotFound";
 
 
 function App() {
-  const [city,setCities] = useState({})
+  const [city, setCities] = useState({});
   useEffect(() => {
     cityApi.getAll().then(res => {
       //console.log(res);
-      setCities(res.data)
-    })
-  },[]);
+      setCities(res.data);
+    });
+  }, []);
 
-  console.log(city)
+  console.log(city);
   // useEffect(() => {
   //   studentApi.getAll().then(res => console.log(res))
   // })
   return (
     <div className="App">
       <Routes>
-        <Route  path="/login" element={<LoginPage/>} />     
-        <PrivateRoute path="/admin" element={ <AdminLayout/>} />      
-        <Route path="*" element={<NotFound/>}/>
-        
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<PrivateRoute />}>
+          <Route path="/admin" element={<AdminLayout />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+
       </Routes>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- src/components/Common/PrivateRoute.tsx
+++ src/components/Common/PrivateRoute.tsx
@@ -1,14 +1,13 @@
+import * as React from "react";
+import { RouteProps, Outlet, Navigate } from "react-router-dom";
 
-import * as React from 'react';
-import { Route, RouteProps } from 'react-router-dom';
 
-
-export default function PrivateRoute (props:  RouteProps) {
-    //check user is logged in
-    //show route
-    //rediract login page
-    const accessToken = Boolean(localStorage.getItem('access_token')); 
-    console.log("accessToken is:",accessToken)
-  return <Route {...props}/>;
+export default function PrivateRoute(props: RouteProps) {
+  //check user is logged in
+  //show route
+  //rediract login page
+  const accessToken = Boolean(localStorage.getItem("access_token"));
+  console.log("accessToken is:", accessToken);
+  return accessToken ? <Outlet /> : <Navigate to="/login" />;
 
 }
